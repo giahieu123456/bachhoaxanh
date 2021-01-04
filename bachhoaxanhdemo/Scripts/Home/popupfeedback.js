270b49ce105e222045d9c43a5cf1546418bbf604
phamminhtuan1999 <phamminhtuan1999@gmail.com>
2021-01-04 14:31:18 +0700

    v1

---
+++ bachhoaxanhdemo/Scripts/Home/popupfeedback.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+	$(".img-feedback").click(function () {
+		$(".popup-cmt").show()
+	})
+
+	$(".close-cmt").click(function () {
+		$(".popup-cmt").hide()
+	})
+	$(window).on('click', function (e) {
+		if ($(e.target).is('.popup-cmt')) {
+
+			$(".popup-cmt").hide()
+		}
+		if ($(e.target).is('.popup-soldout')) {
+
+			$(".popup-soldout").hide()
+		}
+		if ($(e.target).is('.popup-delete')) {
+
+			$(".popup-delete").hide()
+		}
+	})
+})
